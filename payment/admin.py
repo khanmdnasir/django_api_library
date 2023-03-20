2acfdf3866177a9d90c4b9d25b1f783ff1dae00e
Nasir Khan <nasir@aslgroup.com.bd>
2023-03-20 10:27:29 +0600

    notification modified

--- payment/admin.py
+++ payment/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import *
-
+from import_export.admin import ImportExportMixin
 
 # Register your models here.
 admin.site.register(CurrencyModel)
 admin.site.register(PaymentGatewayModel)
-admin.site.register(OrderModel)
+
+class OrderAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['amount', 'customer_name', 'status']
+
+admin.site.register(OrderModel,OrderAdmin)
